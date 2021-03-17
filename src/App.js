1ded56c500032c1e193d4f192b14a8bb478ebecb
Robert Laws <roblaws@me.com>
2021-03-17 17:00:33 +0300

    begin add logic for drag and drop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import DragAndDrop from './components/DragAndDrop';
 // import LessonForm from './components/LessonForm';
 
@@ -7,7 +9,9 @@ function App() {
     <div className='App'>
       <h1>App</h1>
       {/* <LessonForm /> */}
-      <DragAndDrop />
+      <DndProvider backend={HTML5Backend}>
+        <DragAndDrop />
+      </DndProvider>
     </div>
   );
 }
--- src/components/DragAndDrop.js
+++ src/components/DragAndDrop.js
@@ -1,4 +1,6 @@
 import React, { useState, useCallback, useContext, useEffect } from 'react';
+import { Card } from './Card';
+import update from 'immutability-helper';
 import CheckBoxList from './CheckBoxList';
 import OptionsContext from '../context/options/optionsContext';
 
@@ -9,18 +11,67 @@ const DragAndDrop = () => {
   const [formValues, setFormValues] = useState({});
   const [modulesList, setModulesList] = useState([]);
 
+  const style = {
+    width: 400,
+  };
+
+  const moveCard = useCallback(
+    (dragIndex, hoverIndex) => {
+      const dragCard = modulesList[dragIndex];
+      setModulesList(
+        update(modulesList, {
+          $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, dragCard],
+          ],
+        })
+      );
+    },
+    [modulesList]
+  );
+
+  const renderCard = (card, index) => {
+    return (
+      <Card
+        key={card.id}
+        index={index}
+        id={card.id}
+        text={card.name}
+        moveCard={moveCard}
+      />
+    );
+  };
+
   useEffect(() => {
     getModules();
   }, [getModules]);
 
   useEffect(() => {
     if (formValues.modules?.length > 0) {
-      let myModules = formValues.modules.map((number) =>
-        modules.find((module) => module.id === number)
-      );
+      const checkedLength = formValues.modules.length;
+      const modulesListLength = modulesList.length;
 
-      let myModulesNames = myModules.map((module) => module.name);
-      setModulesList(myModulesNames);
+      // console.log(checkedLength);
+      // console.log(modulesListLength);
+
+      if (checkedLength > modulesListLength) {
+        formValues.modules.forEach((item) => {
+          if (modulesList.some((i) => i.id === item)) {
+          } else {
+            let addedModule = modules.find((formItem) => formItem.id === item);
+            setModulesList((prevState) => [...prevState, addedModule]);
+          }
+        });
+      } else {
+        console.log('need to remove one');
+      }
+      // let myModules = formValues.modules.map((number) =>
+      //   modules.find((module) => module.id === number)
+      // );
+      // console.log(myModules);
+
+      // let myModulesNames = myModules.map((module) => module);
+      // setModulesList(myModulesNames);
     } else {
       setModulesList([]);
     }
@@ -43,9 +94,11 @@ const DragAndDrop = () => {
         />
       )}
       <hr />
-      {modulesList &&
-        modulesList.length > 0 &&
-        modulesList.map((module) => <div key={module}>{module}</div>)}
+      {modulesList && modulesList.length > 0 && (
+        <div style={style}>
+          {modulesList.map((module, i) => renderCard(module, i))}
+        </div>
+      )}
     </div>
   );
 };
